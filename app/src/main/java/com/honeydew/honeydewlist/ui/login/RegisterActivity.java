1f5739c2d6a472aaf52776bb7db5e5188f6056b5
getBoolean <danielwnewville@gmail.com>
2021-04-05 19:00:10 -0500

    Add back button

--- app/src/main/java/com/honeydew/honeydewlist/ui/login/RegisterActivity.java
+++ app/src/main/java/com/honeydew/honeydewlist/ui/login/RegisterActivity.java
@@ -1,8 +1,12 @@
 package com.honeydew.honeydewlist.ui.login;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.honeydew.honeydewlist.R;
 
@@ -12,5 +16,20 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // For back button
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // For back button
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
