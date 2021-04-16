0a6e9a6d58b98ccd2c9ea9112c0f4731d7d862a9
getBoolean <danielwnewville@gmail.com>
2021-04-15 22:27:44 -0500

    move setLabel

--- app/src/main/java/com/honeydew/honeydewlist/data/Item.java
+++ app/src/main/java/com/honeydew/honeydewlist/data/Item.java
@@ -4,4 +4,22 @@ abstract public class Item {
     protected String label = "";
     protected String description = "";
     protected boolean completed = false;
+    Item(String label, String description, boolean completed) {
+        this.label = label;
+        this.description = description;
+        this.completed = completed;
+    }
+
+
+    /**
+     * setLabel
+     * @param label the name of task
+     * @return success Return a boolean value indicating if it was successful
+     */
+    protected boolean setLabel(String label) {
+        // Could check name against blacklisted words
+        // Then return false if there is
+        this.label = label;
+        return true;
+    }
 }
--- app/src/main/java/com/honeydew/honeydewlist/data/Task.java
+++ app/src/main/java/com/honeydew/honeydewlist/data/Task.java
@@ -5,22 +5,8 @@ import java.util.HashMap;
 public class Task extends Item {
     private HashMap<String, Double> steps;
     public Task(String label, String description, HashMap<String, Double> steps, boolean completed) {
-        this.label = label;
-        this.description = description;
+        super(label, description, completed);
         this.steps = steps;
-        this.completed = completed;
-    }
-
-    /**
-     * setLabel
-     * @param label the name of task
-     * @return success Return a boolean value indicating if it was successful
-     */
-    public boolean setLabel(String label) {
-        // Could check name against blacklisted words
-        // Then return false if there is
-        this.label = label;
-        return true;
     }
 
 
