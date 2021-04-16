05d0ce130a7b2ae6b037171df06802acca01af47
getBoolean <danielwnewville@gmail.com>
2021-04-16 11:07:24 -0500

    update data classes with getters and setters

--- app/src/main/java/com/honeydew/honeydewlist/data/Item.java
+++ app/src/main/java/com/honeydew/honeydewlist/data/Item.java
@@ -1,32 +1,46 @@
 package com.honeydew.honeydewlist.data;
 
 abstract public class Item {
-    protected String label = "";
-    protected String description = "";
-    protected boolean completed = false;
-    protected Item(String label, String description, boolean completed) {
-        this.label = label;
+    private String name = "";
+    private String owner = "";
+    private String description = "";
+    protected Item(String name, String description) {
+        this.name = name;
         this.description = description;
-        this.completed = completed;
     }
 
 
     /**
      * setLabel
-     * @param label the name of task
+     * @param name the name of task
      * @return success Return a boolean value indicating if it was successful
      */
-    protected boolean setLabel(String label) {
+    protected Boolean setLabel(String name) {
         // Could check name against blacklisted words
         // Then return false if there is
-        this.label = label;
+        this.name = name;
         return true;
     }
 
     /**
-     * @return label
+     * getLabel
+     * @return name
      */
     protected String getLabel() {
-        return this.label;
+        return this.name;
+    }
+
+    /**
+     * getDescription
+     * @return description
+     */
+    protected String getDescription() { return this.description; }
+
+    protected String getOwner() {
+        return owner;
+    }
+
+    protected void setOwner(String owner) {
+        this.owner = owner;
     }
 }
--- app/src/main/java/com/honeydew/honeydewlist/data/Task.java
+++ app/src/main/java/com/honeydew/honeydewlist/data/Task.java
@@ -1,13 +1,39 @@
 package com.honeydew.honeydewlist.data;
 
-import java.util.HashMap;
+import java.util.List;
 
 public class Task extends Item {
-    private HashMap<String, Double> steps;
-    public Task(String label, String description, HashMap<String, Double> steps, boolean completed) {
-        super(label, description, completed);
+    private List<String> steps;
+    private long points;
+    private Boolean completed = false;
+    public Task(String name, String description, long points, List<String> steps, Boolean completed) {
+        super(name, description);
+        this.steps = steps;
+        this.points = points;
+        this.completed = completed;
+    }
+
+    protected List<String> getSteps() {
+        return steps;
+    }
+
+    protected void setSteps(List<String> steps) {
         this.steps = steps;
     }
 
+    protected long getPoints() {
+        return points;
+    }
 
+    protected void setPoints(long points) {
+        this.points = points;
+    }
+
+    protected Boolean getCompleted() {
+        return completed;
+    }
+
+    protected void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
 }
