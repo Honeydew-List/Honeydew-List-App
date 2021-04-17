7a82eab1b9e1788ac27b0e9c7a98264c9c46065d
Boolean <danielwnewville@gmail.com>
2021-04-17 01:46:22 -0500

    Fix: Rename completed to completionStatus
    Must be the same as in Firestore

--- app/src/main/java/com/honeydew/honeydewlist/data/Task.java
+++ app/src/main/java/com/honeydew/honeydewlist/data/Task.java
@@ -2,21 +2,25 @@ package com.honeydew.honeydewlist.data;
 
 import java.util.List;
 
+/**
+ * Task
+ * Note that the variable names must be the same as what is in Firestore
+ */
 public class Task extends Item {
     private List<String> steps;
     private Long points;
-    private Boolean completed = false;
+    private Boolean completionStatus = false;
 
     // Required for FireStore
     public Task() {
         super(); // Required for firestore
     }
 
-    public Task(String name, String description, String owner, String uuid, Long points, List<String> steps, Boolean completed) {
+    public Task(String name, String description, String owner, String uuid, Long points, List<String> steps, Boolean completionStatus) {
         super(name, description, owner, uuid);
         this.steps = steps;
         this.points = points;
-        this.completed = completed;
+        this.completionStatus = completionStatus;
     }
 
     public List<String> getSteps() {
@@ -35,11 +39,11 @@ public class Task extends Item {
         this.points = points;
     }
 
-    public Boolean getCompleted() {
-        return completed;
+    public Boolean getCompletionStatus() {
+        return completionStatus;
     }
 
-    public void setCompleted(Boolean completed) {
-        this.completed = completed;
+    public void setCompletionStatus(Boolean completionStatus) {
+        this.completionStatus = completionStatus;
     }
 }
