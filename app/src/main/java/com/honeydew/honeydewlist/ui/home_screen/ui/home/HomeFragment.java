d149bb9007103f49c5a44afdfc7032a84210398b
Skylockwx <81264080+Skylockwx@users.noreply.github.com>
2021-04-24 15:22:37 -0500

    Added Home Screen Username pull and Melon Count pull.

--- app/src/main/java/com/honeydew/honeydewlist/ui/home_screen/ui/home/HomeFragment.java
+++ app/src/main/java/com/honeydew/honeydewlist/ui/home_screen/ui/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,38 +16,59 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.honeydew.honeydewlist.R;
 import com.honeydew.honeydewlist.ui.login.LoginActivity;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private FirebaseFirestore db;
+    private String userID;
+    private String username,melons;
+    private TextView userHome,Melons;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         // Temporary logout button
         Button logout;
         logout = (Button) root.findViewById(R.id.button);
 
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(getContext(), LoginActivity.class);
-                startActivity(i);
-                getActivity().finish();
-            }
+        userHome = (TextView) root.findViewById(R.id.userHome);
+        Melons = (TextView) root.findViewById(R.id.melonHome);
+
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseUser user = auth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        if (user != null) {
+            userID = user.getUid();
+            db.collection("users").document(userID).get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        username = documentSnapshot.getData().get("username").toString();
+                        userHome.setText(username);
+                    })
+                    .addOnFailureListener(e -> Toast.makeText(requireContext(),
+                            "Could not find username from Firestore",
+                            Toast.LENGTH_SHORT).show());
+
+            db.collection("users").document(userID).get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        melons = documentSnapshot.getData().get("melon_count").toString();
+                        Melons.setText(getString(R.string.melonText, melons));
+                    })
+                    .addOnFailureListener(e -> Toast.makeText(requireContext(),
+                            "Could not find melon count from Firestore",
+                            Toast.LENGTH_SHORT).show());
+
+        }
+
+        logout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent i = new Intent(getContext(), LoginActivity.class);
+            startActivity(i);
+            getActivity().finish();
         });
         return root;
     }
