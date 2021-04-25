b70957052d1bee43d0508afddfe600339ada4467
arwajafferji <arwajafferji110@gmail.com>
2021-04-25 13:56:05 -0500

    friends changes

--- app/src/main/java/com/honeydew/honeydewlist/ui/home_screen/HomeScreen.java
+++ app/src/main/java/com/honeydew/honeydewlist/ui/home_screen/HomeScreen.java
@@ -24,7 +24,7 @@ public class HomeScreen extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_tasks, R.id.navigation_rewards,R.id.navigation_options)
+                R.id.navigation_home, R.id.navigation_tasks, R.id.navigation_rewards,R.id.navigation_options, R.id.navigation_friends)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
