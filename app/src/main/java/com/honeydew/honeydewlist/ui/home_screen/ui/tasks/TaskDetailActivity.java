75bdf415bcc1acd8ac83d89551277af79b26a866
getBoolean <danielwnewville@gmail.com>
2021-04-25 14:42:49 -0500

    Update data from using firebase

--- app/src/main/java/com/honeydew/honeydewlist/ui/home_screen/ui/tasks/TaskDetailActivity.java
+++ app/src/main/java/com/honeydew/honeydewlist/ui/home_screen/ui/tasks/TaskDetailActivity.java
@@ -11,15 +11,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.honeydew.honeydewlist.R;
 
 import java.text.MessageFormat;
 
 public class TaskDetailActivity extends AppCompatActivity {
+    private static final String TAG = "Firestore";
+    private FirebaseFirestore db;
+    private Boolean completionStatus, verifiedStatus;
+    private String taskName, taskDescription, taskOwner, taskOwnerUUID, taskID;
+    private long melonReward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +36,8 @@ public class TaskDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Declare variables
-        String taskName, taskDescription, taskOwner, taskOwnerUUID, taskID;
-        Long melonReward;
-        Boolean completionStatus, verifiedStatus;
-        TextView reward_tv, owner_tv, description_tv;
+        TextView status_tv, reward_tv, owner_tv, description_tv;
+        db = FirebaseFirestore.getInstance();
 
         Intent i = getIntent();
 
@@ -61,13 +66,70 @@ public class TaskDetailActivity extends AppCompatActivity {
         verifiedStatus = i.getBooleanExtra("verifiedStatus", false);
 
         // Find text views
+        status_tv = findViewById(R.id.status);
         reward_tv = findViewById(R.id.reward);
         owner_tv = findViewById(R.id.owner);
         description_tv = findViewById(R.id.description);
 
         // Set text views to data from intent extras
+        if (completionStatus) {
+            if (verifiedStatus) {
+                status_tv.setText(R.string.VerifiedStatusText);
+            } else {
+                status_tv.setText(R.string.CompletedStatusText);
+            }
+        } else {
+            status_tv.setText(R.string.NotCompletedText);
+        }
+
         reward_tv.setText(MessageFormat.format("{0}🍈", melonReward));
         owner_tv.setText(taskOwner);
         description_tv.setText(taskDescription);
+
+        db.collection("users").document(taskOwnerUUID).collection("tasks").document(taskID).addSnapshotListener((value, error) -> {
+            try {
+                completionStatus = value.getBoolean("completionStatus");
+                verifiedStatus = value.getBoolean("verifiedStatus");
+                taskDescription = value.getData().get("description").toString();
+                taskOwner = value.getData().get("owner").toString();
+                taskOwnerUUID = value.getData().get("uuid").toString();
+                melonReward = value.getLong("points");
+
+                // Update text views
+                if (completionStatus) {
+                    if (verifiedStatus) {
+                        status_tv.setText(R.string.VerifiedStatusText);
+                    } else {
+                        status_tv.setText(R.string.CompletedStatusText);
+                    }
+                } else {
+                    status_tv.setText(R.string.NotCompletedText);
+                }
+
+                reward_tv.setText(MessageFormat.format("{0}🍈", melonReward));
+                owner_tv.setText(taskOwner);
+                description_tv.setText(taskDescription);
+            } catch (RuntimeException e) {
+                Log.e(TAG, "onCreate: Maybe document field is not the right type?", e);
+            } catch (Exception e) {
+                Log.e(TAG, "onCreate: Something happened", e);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (db != null) {
+            db.terminate();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (db != null) {
+            db.terminate();
+        }
     }
 }
